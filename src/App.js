9ccdb78bab7ee04a671adb5d7a27df1928f03b20
github username <github email address>
2020-05-30 23:56:04 -0400

    implemented navigation bar html

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="navigation">
+        <div className="navigation__logo">Triona Moynihan</div>
+        <ul className="navigation__list">
+          <li className="navigation__list-item"> About </li>
+          <li className="navigation__list-item"> Projects </li>
+          <li className="navigation__list-item"> Contact </li>
+        </ul>
+      </nav>
       <header className="App-header">
         <p>
           My Portfolio
