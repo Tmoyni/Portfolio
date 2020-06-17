f8cdb7ad8f80da688a4b852e5a791d004f87f723
github username <github email address>
2020-06-17 17:34:32 -0400

    added react scroll to document, still haveing problems with section scrolling, but logo works

--- src/App.js
+++ src/App.js
@@ -2,40 +2,74 @@ import React from 'react';
 import './App.css';
 import ProjectContainer from './Containers/ProjectContainer'
 import ContactContainer from './Containers/ContactContainer';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <nav className="navigation">
-        <div className="navigation__logo">Triona Moynihan</div>
-        <ul className="navigation__list">
-          <li className="navigation__list-item">About</li>
-          <li className="navigation__list-item">Projects</li>
-          <li className="navigation__list-item">Contact</li>
-        </ul>
-      </nav>
+class App extends React.Component {
+// function App() {
 
-      <header className="intro">
-        <figure className="intro__shape">
-          <img src="https://images.squarespace-cdn.com/content/52845256e4b0ecd5a8451e07/1411001501633-45ZERF22Y3ZJ9N4VEGLW/Triona.jpg?format=500w&content-type=image%2Fjpeg" alt="" className="intro__image"/> 
-        </figure>
-        <div className="intro__details">
-          <div className="intro__about">Hi, I'm <span className="bld-color">Triona Moynihan</span> and I am a New York City based full-stack web developer and designer.</div>
-          <div className="intro__links">
-            <a className="intro__link" href="https://github.com/Tmoyni" target="_blank" >Github</a>
-            <a className="intro__link" href="https://www.linkedin.com/in/trionamoynihan/" target="_blank" >LinkedIn</a>
-          </div>
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <nav className="navigation">
+            <div className="navigation__logo" onClick={this.scrollToTop}>Triona Moynihan</div>
+
+            <ul className="navigation__list">
+              <li className="navigation__list-item">
+                <Link
+                  activeClass="active"
+                  to="intro"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500} >
+                </Link>About</li>
+              <li className="navigation__list-item">
+                <Link
+                  activeClass="active"
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500} >
+                </Link>Projects</li>
+              <li className="navigation__list-item">
+                <Link
+                  activeClass="active"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500} >
+                </Link>Contact</li>
+            </ul>
+          </nav>
+
+          <header className="intro" id="intro"> 
+            <figure className="intro__shape">
+              <img src="https://images.squarespace-cdn.com/content/52845256e4b0ecd5a8451e07/1411001501633-45ZERF22Y3ZJ9N4VEGLW/Triona.jpg?format=500w&content-type=image%2Fjpeg" alt="" className="intro__image"/> 
+            </figure>
+            <div className="intro__details">
+              <div className="intro__about">Hi, I'm <span className="bld-color">Triona Moynihan</span> and I am a New York City based full-stack web developer and designer.</div>
+              <div className="intro__links">
+                <a className="intro__link" href="https://github.com/Tmoyni" target="_blank" >Github</a>
+                <a className="intro__link" href="https://www.linkedin.com/in/trionamoynihan/" target="_blank" >LinkedIn</a>
+              </div>
+            </div>
+          </header>
+
+          <div className="section__headline">Projects</div>
+          <ProjectContainer id="projects"/>
+          <div className="section__headline bg--green">Contact</div>
+          <ContactContainer id="contact"/>
         </div>
-      </header>
-
-      <div className="section__headline">Projects</div>
-      <ProjectContainer />
-      <div className="section__headline bg--green">Contact</div>
-      <ContactContainer/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
