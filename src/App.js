1922a690339bf89727d10993bbe16d0d44e341db
github username <github email address>
2020-06-20 16:23:38 -0400

    editing styles to buttons plus color scheme, added author etc to package.json

--- src/App.js
+++ src/App.js
@@ -28,18 +28,23 @@ class App extends React.Component {
           </nav>
 
           <header className="intro" name="intro" ref={(section) => { this.Intro = section; }}> 
-            {/* <div className="intro__content"> */}
               <figure className="intro__shape">
                 <img src="https://images.squarespace-cdn.com/content/52845256e4b0ecd5a8451e07/1411001501633-45ZERF22Y3ZJ9N4VEGLW/Triona.jpg?format=500w&content-type=image%2Fjpeg" alt="" className="intro__image"/> 
               </figure>
               <div className="intro__details">
                 <div className="intro__about">Hi, I'm <span className="bld-color">Triona Moynihan</span> and I am a New York City based full-stack web developer and designer.</div>
                 <div className="intro__links">
-                  <a className="intro__link" href="https://github.com/Tmoyni" target="_blank" >Github</a>
+                  <a className="intro__link" href="https://github.com/Tmoyni" target="_blank" >
+                    {/* <div className="intro__icon">
+                      <svg>
+                        <use xlinkHref="/img/linkedin.svg" />
+                      </svg>
+                    </div> */}
+                    Github
+                  </a>
                   <a className="intro__link" href="https://www.linkedin.com/in/trionamoynihan/" target="_blank" >LinkedIn</a>
                 </div>
               </div>
-            {/* </div> */}
           </header>
           
           <section className="section__headline" name="projects" ref={(section) => { this.Projects = section; }}>Projects</section>
