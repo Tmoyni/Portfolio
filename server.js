bf891b1272f85f34e3a3d12a0e074ac2c26cd71c
github username <github email address>
2020-06-28 17:31:43 -0400

    updated

--- server.js
+++ server.js
@@ -14,7 +14,20 @@
 //    console.log('Server is up!');
 // });
 
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+
+
+// var express = require('express');
+// var app = express();
+// app.use(express.static(__dirname + '/'));
+// app.listen(process.env.PORT || 8080);
+
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(9000);
