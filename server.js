3ec6ad0bde23cfa12f0a46480f40911436de940c
github username <github email address>
2020-06-28 16:31:30 -0400

    fix for server

--- server.js
+++ server.js
@@ -13,3 +13,8 @@
 // app.listen(port, () => {
 //    console.log('Server is up!');
 // });
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
