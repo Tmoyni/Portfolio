0a96fedd38b4696b65f6240e83b40f2c23d86323
github username <github email address>
2020-07-10 18:07:07 -0400

    updated images for projects and fixed opening in a new tab bug

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Natours from '../Images/Natours.jpg';
+import Trillo from '../Images/Trillo.jpg';
+import FantasyStocks from '../Images/FantasyStocks.png';
+import Nexter from '../Images/Nexter.jpg';
+import Portfolio from '../Images/Portfolio.jpg';
+import PostManager from '../Images/PostManager.jpg';
 
 class Project extends React.Component {
 
@@ -12,8 +18,8 @@ class Project extends React.Component {
                 <div className="project__details">{this.props.project.details}</div>
                 <div className="project__tech">{this.props.project.tech}</div>
                 <div className="project__links">
-                    <a className="project__link" href={this.props.project.url} >View</a>
-                    <a className="project__link" href={this.props.project.github} >Github</a>
+                    <a className="project__link" href={this.props.project.url} target="_blank" rel="noopener noreferrer">View</a>
+                    <a className="project__link" href={this.props.project.github} target="_blank" rel="noopener noreferrer">Github</a>
                 </div>
           </div>
         );
