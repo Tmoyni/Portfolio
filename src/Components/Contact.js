0bb95d64a0231fe479b01d53a705f6a34be81a25
github username <github email address>
2021-01-04 22:55:14 -0500

    updated env vars

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,9 +3,9 @@ import emailjs from 'emailjs-com';
 
 // Email validation
 const emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
-const EMAILJS_SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID
-const EMAILJS_TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
-const EMAILJS_USERID = process.env.REACT_APP_EMAILJS_USERID
+const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID
+const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID
+const EMAILJS_USERID = process.env.EMAILJS_USERID
 
 
 // Form validation
